9e2be45f700cefb293fb93efce20b772682a159d
kostr2010 <kostr2010@gmail.com>
2020-02-11 18:24:02 +0300

    more comments to the god of comments

--- map.h
+++ map.h
@@ -7,15 +7,32 @@
 // STRUCTURES
 
 struct _Map {
+    // deallocates all memory, associated with object
     void  (*destroy)(struct _Map* obj);
+
+    // inserts given key and value to the object
     int   (*insert)(struct _Map* obj, void* key, void* value);
+
+    // deletes element with given key
     int   (*delete)(struct _Map* obj, void* key);
+
+    // changes value of given key to the newValue
     int   (*change)(struct _Map* obj, void* key, void* newValue);
+
+    // returns value of given key
     void* (*get)(struct _Map* obj, void* key);
+
+    // prints key
     void  (*print_key)(void* key);
+
+    // prints value
     void  (*print_value)(void* value);
+
+    // counts all elements with given value in the object
     int   (*count_value)(struct _Map* obj, void* value);
-	int (*size)(struct _Map* obj);
+    
+    // returns number of elemnts in the object
+	int   (*size)(struct _Map* obj);
 };
 typedef struct _Map Map;
 
