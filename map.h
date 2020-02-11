1948649dabf167a9b6ee5e95f42b23bc904bb3c6
kostr2010 <kostr2010@gmail.com>
2020-02-11 18:26:32 +0300

    more comments for the god of comments

--- map.h
+++ map.h
@@ -7,32 +7,15 @@
 // STRUCTURES
 
 struct _Map {
-    // deallocates all memory, associated with object
-    void  (*destroy)(struct _Map* obj);
-
-    // inserts given key and value to the object
-    int   (*insert)(struct _Map* obj, void* key, void* value);
-
-    // deletes element with given key
-    int   (*delete)(struct _Map* obj, void* key);
-
-    // changes value of given key to the newValue
-    int   (*change)(struct _Map* obj, void* key, void* newValue);
-
-    // returns value of given key
-    void* (*get)(struct _Map* obj, void* key);
-
-    // prints key
-    void  (*print_key)(void* key);
-
-    // prints value
-    void  (*print_value)(void* value);
-
-    // counts all elements with given value in the object
-    int   (*count_value)(struct _Map* obj, void* value);
-    
-    // returns number of elemnts in the object
-	int   (*size)(struct _Map* obj);
+    void  (*destroy)(struct _Map* obj);                             // deallocates all memory, associated with object
+    int   (*insert)(struct _Map* obj, void* key, void* value);      // inserts given key and value to the object
+    int   (*delete)(struct _Map* obj, void* key);                   // deletes element with given key
+    int   (*change)(struct _Map* obj, void* key, void* newValue);   // changes value of given key to the newValue
+    void* (*get)(struct _Map* obj, void* key);                      // returns value of given key
+    void  (*print_key)(void* key);                                  // prints key
+    void  (*print_value)(void* value);                              // prints value
+    int   (*count_value)(struct _Map* obj, void* value);            // counts all elements with given value in the object
+	int   (*size)(struct _Map* obj);                                // returns number of elemnts in the object
 };
 typedef struct _Map Map;
 
