bb7ed0a5c2548539fb215b1a80db8d9100bd6f34
Troegubov Alex <troegubov.aiu@phystech.edu>
2020-02-15 19:01:36 +0300

    add const in map.h

--- map.h
+++ map.h
@@ -7,12 +7,12 @@
 // STRUCTURES
 
 struct _Map {
-    void  (*destroy)(struct _Map* obj);                             // deallocates all memory, associated with object
-    int   (*insert)(struct _Map* obj, void* key, void* value);      // inserts given key and value to the object
-    int   (*delete)(struct _Map* obj, void* key);                   // deletes element with given key
-    int   (*change)(struct _Map* obj, void* key, void* newValue);   // changes value of given key to the newValue
-    void* (*get)(struct _Map* obj, void* key);                      // returns value of given key
-    int   (*count_value)(struct _Map* obj, void* value);            // counts all elements with given value in the object
-	int   (*size)(struct _Map* obj);                                // returns number of elements in the object
+    void  (*destroy)(struct _Map* obj);                             		// deallocates all memory, associated with obj
+    int   (*insert)(struct _Map* obj, const void* key, const void* value);	// inserts given key and value to the object
+    int   (*delete)(struct _Map* obj, const void* key);                   	// deletes element with given key
+    int   (*change)(struct _Map* obj, const void* key, const void* newValue);// changes value of given key to the newValue
+    void* (*get)(const struct _Map* obj, const void* key);                	// returns value of given key
+    int   (*count_value)(const struct _Map* obj, const void* value);        // counts all elements with given value
+	int   (*size)(const struct _Map* obj);                                	// returns number of elements in the object
 };
 typedef struct _Map Map;
